569da339ce4445cfb3232a8ebbf385671bb88cac
GOLD <robertlove99999@gmail.com>
2022-04-25 16:14:45 -0500

    js done

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('javascript is connected')
+
+
+// define a variable
+let userName = "";
+
+// use the variable in a alert message
+alert("Hello, " + userName+"!");
+
+// prompt the user for input
+
+let home = prompt('Where are you from, '+userName+"?");
+
+alert('It is so nice to meet you, '+userName+"!. I bet "+home+" is a beautiful place")
+
