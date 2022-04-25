94e0d7a0438ae02de601fe9d0be2784a5b6dc375
GOLD <robertlove99999@gmail.com>
2022-04-25 16:20:15 -0500

    added js

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ alert("Hello, " + userName+"!");
 
 let home = prompt('Where are you from, '+userName+"?");
 
-alert('It is so nice to meet you, '+userName+"!. I bet "+home+" is a beautiful place")
+alert('It is so nice to meet you, '+userName+"!. I bet "+home+" is a beautiful place!")
 
